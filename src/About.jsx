fee55514f2b9a233fa60d35e394e5b79cab1974b
Zafe123 <scottpilgrim712@gmail.com>
2023-05-02 17:39:44 +0800

    123

--- src/About.jsx
+++ src/About.jsx
@@ -1,16 +1,34 @@
+import profile from './images/profile.webp';
+
+
 function About() {
   return (
+
     <center>
-      <div className="col-md-5 m-5 p-5 border border-1 rounded-3 shadow">
-        <h1><strong>Group 3</strong></h1>
-        <h6 className="pt-3">"Programming really is hard, might as well rethink continue learning it, or not"</h6>
-        <div className="pt-5">
-          <h4 className="pb-3"><strong>Group Members</strong></h4>
-          <p>Joelyn Ladores</p>
-          <p>Heherson Edra</p>
-          <p>Joshua Zafe</p>
-          <p>Val Everson Sienes</p>
-          <p>Allen Lemuel Claridad</p>
+      <div className="container p-3 border border-1 rounded-3 shadow">
+        <div className="row">
+          <div className="col-md-6 about">
+            <p>My name is Joshua Zafe, and I'm a graduate of Bachelor of Science in Information Technology.
+              After completing my degree, I worked in the call center industry for 2.5 years, where I
+              honed my communication and problem-solving skills. However, I knew that my true passion lies
+              in the field of web development, which is why I decided to enroll in the Kodego Fullstack
+              web development bootcamp.
+            </p>
+
+            <p>During my time at Kodego, I was able to develop my skills in graphics design and web user
+              interface and user experience design, which are my specialties. I am passionate about
+              creating beautiful and intuitive user interfaces that provide a seamless user experience.
+              With my technical background and creativity, I'm confident in my ability to bring ideas to
+              life and contribute to the success of any project I work on.</p>
+
+            <p> Thank you for taking the time to get to know me, and I look forward to the opportunity to
+              work with you.</p>
+
+            <a href="resume/resume.pdf" class="btn btn-danger" target="_blank">VIEW CV</a>
+          </div>
+          <div className="col-md-6">
+            <img src={profile} className="profile" />
+          </div>
         </div>
       </div>
     </center>
